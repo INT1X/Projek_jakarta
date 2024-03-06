01e9aa185a700648aa32ab7eb580f54511fcebb6
INT1X <119658253+INT1X@users.noreply.github.com>
2024-03-06 14:17:16 +0800

    Update Print_nama.py

--- Print_nama.py
+++ Print_nama.py
@@ -1,1 +1,15 @@
-print ("Nama Kelompok Jakarta")
+# Nama-nama yang akan ditampilkan
+nama_list = [
+    "Ari Purnomo Aji",
+    "Aisah",
+    "Jihan Hakima",
+    "Muhammad Farrel Daviaziz",
+    "Dhita Hartanti Octavia",
+    "Ahmad Raihan Asyam",
+    "Fernanda S",
+    "Sendi Alhazen Mulyono"
+]
+
+# Menampilkan nama-nama
+for nama in nama_list:
+    print(nama)
